a76e206aa41bfecf6db121461973e2e620ac27fc
Manuel Boo <33934263+ManuBoo@users.noreply.github.com>
2023-12-11 11:47:44 +0100

    Primer commit
    Primer commit

---
+++ pruebaTecnica/src/main/java/com/manuboo/pruebaTecnica/PruebaTecnicaW2MApplication.java
@@ -0,0 +1,13 @@
+package com.manuboo.pruebaTecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaW2MApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTecnicaW2MApplication.class, args);
+	}
+
+}
---
+++ pruebaTecnica/src/test/java/com/manuboo/pruebaTecnica/PruebaTecnicaW2MApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manuboo.pruebaTecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnicaW2MApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
