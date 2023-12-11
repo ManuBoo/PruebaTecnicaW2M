8c241b50aae951cfe90aa65890c7a044bb33ad53
Manuel Boo <33934263+ManuBoo@users.noreply.github.com>
2023-12-11 19:25:33 +0100

    Documentacion con swagger

--- pruebaTecnica/src/main/java/com/manuboo/pruebaTecnica/PruebaTecnicaW2MApplication.java
+++ pruebaTecnica/src/main/java/com/manuboo/pruebaTecnica/PruebaTecnicaW2MApplication.java
@@ -2,6 +2,11 @@ package com.manuboo.pruebaTecnica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @SpringBootApplication
 public class PruebaTecnicaW2MApplication {
@@ -9,5 +14,17 @@ public class PruebaTecnicaW2MApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PruebaTecnicaW2MApplication.class, args);
 	}
+	
+	 @Bean
+	    public OpenAPI customOpenAPI() {
+	        return new OpenAPI()
+	                .info(new Info()
+	                        .title("API SuperHeroes")
+	                        .version("1.0")
+	                        .description("API SuperHeroes para prueba tecnica de W2M")
+	                        .termsOfService("http://swagger.io/terms/")
+	                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+
+	    }
 
 }
--- pruebaTecnica/src/main/java/com/manuboo/pruebaTecnica/controller/SuperHeroeController.java
+++ pruebaTecnica/src/main/java/com/manuboo/pruebaTecnica/controller/SuperHeroeController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.manuboo.pruebaTecnica.entity.SuperHeroe;
 import com.manuboo.pruebaTecnica.service.SuperHeroesServiceInterface;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 @RestController
 @RequestMapping("/super-heroes")
 public class SuperHeroeController {
@@ -21,26 +23,31 @@ public class SuperHeroeController {
 	@Autowired
     private SuperHeroesServiceInterface superHeroeService;
 
+	@Operation(summary = "Devuelve todos los héroes", description = "Devuelve todos los héroes.")
     @GetMapping
     public List<SuperHeroe> allHeroes() {
         return superHeroeService.allHeroes();
     }
-    
+
+	@Operation(summary = "Busca los héroes que contenga el texto introducido", description = "Busca los héroes que contenga el texto introducido.")
     @GetMapping("/buscar")
 	public List<SuperHeroe> buscarHeroe(@RequestParam(name = "texto") String texto){
 		return superHeroeService.buscar(texto);
 	}
     
+	@Operation(summary = "Devuelve un héroe por ID", description = "Devuelve un héroe por ID.")
     @GetMapping("/heroe")
 	public SuperHeroe findHeroe(@RequestParam(name="id") Integer id) {
 		return  superHeroeService.findbyId(id);
 	}
     
+	@Operation(summary = "Actualiza el nombre de un héroe buscando dicho héroe por ID", description = "Actualiza el nombre de un héroe buscando dicho héroe por ID.")
     @PutMapping
     public SuperHeroe modificarHeroe(@RequestBody SuperHeroe superHeroe){
     	return  superHeroeService.modificarHeroe(superHeroe);
     }
 
+	@Operation(summary = "Borra un héroe", description = "Borra un héroe.")
 	@DeleteMapping
 	public void borrar(@RequestParam(name="id") Integer id) {
 		superHeroeService.borrar(id);
